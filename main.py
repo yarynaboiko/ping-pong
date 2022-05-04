144a80b05c52d7343effa3cf54863b3141c615bd
yarynaboiko <103835771+yarynaboiko@users.noreply.github.com>
2022-05-04 19:53:12 +0300

    simple pygame window

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print ("Hello world")
+from pygame import *
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+display.set_caption('Ping pong')
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
